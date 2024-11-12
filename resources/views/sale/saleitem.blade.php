ea70ed1111fb86ed9fd93ce3c0807b49ba029d80
Ammar123729 <marriamarr345@gmail.com>
2024-11-12 10:03:00 +0500

    setup the form

--- resources/views/sale/saleitem.blade.php
+++ resources/views/sale/saleitem.blade.php
@@ -42,7 +42,7 @@
     </a>
 
     <a href="{{url('dashboard')}}" class="btn btn-primary">
-        <i class="bi bi-arrow-left mx-2" style="font-size:50px; font-weight:900"></i>
+        New Sale Form <i class="bi bi-arrow-left mx-2" style="font-size:50px; font-weight:900"></i>
     </a>
 
     <!-- partial:partials/_navbar.html -->
