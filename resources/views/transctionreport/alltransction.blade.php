190d277da1c2ba6c9e2f0b68184188f25bfce7ed
Ammar123729 <marriamarr345@gmail.com>
2024-11-13 15:05:33 +0500

    add alltransction add table

--- resources/views/transctionreport/alltransction.blade.php
+++ resources/views/transctionreport/alltransction.blade.php
@@ -494,7 +494,7 @@
                     <div class="col-lg-12 grid-margin stretch-card">
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="card-title">Sale Transction</h4>
+                                <h4 class="card-title">All Transction</h4>
 
                                 <div class="table-responsive">
                                     <table class="table table-striped">
