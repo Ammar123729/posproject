2351a5577b0a94e9279ec6ba90e86bb93f04e419
Ammar123729 <marriamarr345@gmail.com>
2024-12-06 13:58:09 +0500

    item detail responsive

--- resources/views/partydetail/additem.blade.php
+++ resources/views/partydetail/additem.blade.php
@@ -566,8 +566,8 @@
               </div>
             </div>
             <div style="display: block;">
-              <div class="col-md-12 col-sm-12 grid-margin stretch-card aji" style="width: 99%;">
-                <div class="card">
+              <div class="col-md-12 col-sm-12 grid-margin stretch-card " style="width: 99%;">
+                <div class="card aji">
                   <div class="row">
                     @if(isset($selectitem))
                     <div class="col-md-7">
