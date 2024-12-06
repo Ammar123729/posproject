bbfaadb454f68a9306fda3d7419f03c386bd7c40
Ammar123729 <marriamarr345@gmail.com>
2024-12-06 13:44:31 +0500

    item detail responsive

--- resources/views/partydetail/additem.blade.php
+++ resources/views/partydetail/additem.blade.php
@@ -498,7 +498,7 @@
         <div class="row">
 
           <div style="display: flex; " class="adj">
-            <div class="col-xl-4 col-md-12 col-sm-12 grid-margin stretch-card" style="margin-right: 20px;">
+            <div class="col-xl-4 col-md-12 col-sm-12 grid-margin stretch-card med" style="margin-right: 20px;">
               <div class="card">
                 <div class="card-body">
                   <!-- <div class="text-end">
